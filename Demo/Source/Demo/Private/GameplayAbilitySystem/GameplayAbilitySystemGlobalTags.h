cb5af832eafc7c672581534c68e9716a0eb5f56a
liudong <l1206776367@yahoo.com>
2024-05-17 01:47:20 +0800

    gas tags

--- Demo/Source/Demo/Private/GameplayAbilitySystem/GameplayAbilitySystemGlobalTags.h
+++ Demo/Source/Demo/Private/GameplayAbilitySystem/GameplayAbilitySystemGlobalTags.h
@@ -53,37 +53,58 @@ struct DEMO_API FGameplayAbilityGlobalTags final : public FGameplayTagNativeAdde
 		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Event.TakeHit.Melee"), TEXT("ProjectD"));
 		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Event.TakeHit.Remote"), TEXT("ProjectD"));
 		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Event.TakeHit.Crit"), TEXT("ProjectD"));
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Event.bRestricted.bRooted"), TEXT("ProjectD"));
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Event.bRestricted.bParalyzed"), TEXT("ProjectD"));
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Event.bRestricted.bSilenced"), TEXT("ProjectD"));
 		
 		Event_TakeDamage = UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Event.TakeDamage"), TEXT("ProjectD"));
-		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Event.TakeDamage.Phy"), TEXT("ProjectD"));
-		//UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Event.TakeDamage.Arc"), TEXT("ProjectD"));
-		//UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Event.TakeDamage.Frost"), TEXT("ProjectD"));
-		//UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Event.TakeDamage.Flame"), TEXT("ProjectD"));
-		//UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Event.TakeDamage.Divine"), TEXT("ProjectD"));
-		//UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Event.TakeDamage.Shadow"), TEXT("ProjectD"));
-		//UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Event.TakeDamage.Nature"), TEXT("ProjectD"));
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Event.TakeDamage"), TEXT("ProjectD"));
 
-		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Category.Buff"), TEXT("ProjectD"));
-		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Category.DeBuff"), TEXT("ProjectD"));
-		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Category.Limit"), TEXT("ProjectD"));
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Category1.Buff"), TEXT("ProjectD"));
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Category1.DeBuff"), TEXT("ProjectD"));
+
+		// Buff类型
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Category2.None"), TEXT("ProjectD"));
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Category2.Magic"), TEXT("ProjectD"));
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Category2.Curse"), TEXT("ProjectD"));
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Category2.Disease"), TEXT("ProjectD"));
+
+		// 限制类buff标签
+		Paralysis = UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Restrict.Paralysis"), TEXT("ProjectD"));
+		Stun	= UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Restrict.Paralysis.Stun"), TEXT("ProjectD"));
+		Sleep	= UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Restrict.Paralysis.Sleep"), TEXT("ProjectD"));
+		Fear	= UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Restrict.Paralysis.Fear"), TEXT("ProjectD"));
+		Root	= UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Restrict.Root"), TEXT("ProjectD"));
+		Silence	= UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Restrict.Silence"), TEXT("ProjectD"));
+
+		// 伤害类标签
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.DamageType.Phy"), TEXT("ProjectD"));
+		//UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.DamageType.Arc"), TEXT("ProjectD"));
+		//UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.DamageType.Frost"), TEXT("ProjectD"));
+		//UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.DamageType.Flame"), TEXT("ProjectD"));
+		//UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.DamageType.Divine"), TEXT("ProjectD"));
+		//UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.DamageType.Shadow"), TEXT("ProjectD"));
+		//UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.DamageType.Nature"), TEXT("ProjectD"));
+
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Ability.Category1.Action"), TEXT("ProjectD"));
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Ability.Category1.Reaction"), TEXT("ProjectD"));
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Ability.Category1.AdditionalAction"), TEXT("ProjectD"));
+
+		// 专注技能
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Ability.Focus"), TEXT("ProjectD"));
+
 		
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Ability.Category2.Combat"), TEXT("ProjectD"));
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Ability.Category2.Spell"), TEXT("ProjectD"));
+
+		// 各职业三系流派
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Ability.Category2.Spell.Frost"), TEXT("ProjectD"));
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Ability.Category2.Spell.Flame"), TEXT("ProjectD"));
+		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Ability.Category2.Spell.Arc"), TEXT("ProjectD"));
+		
+		Move = UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Ability.Category.Movement.Move"), TEXT("ProjectD"));
+
 		InBattle	= UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.State.InBattle"), TEXT("ProjectD"));
-		
-		Paralysis	= UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Limit.Paralysis"), TEXT("ProjectD"));
-		Stun		= UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Limit.Paralysis.Stun"), TEXT("ProjectD"));
-		Sleep		= UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Limit.Paralysis.Sleep"), TEXT("ProjectD"));
-		Fear		= UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Limit.Paralysis.Fear"), TEXT("ProjectD"));
-
-		Root		= UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Limit.Root"), TEXT("ProjectD"));
-		Silence		= UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.GE.Limit.Silence"), TEXT("ProjectD"));
-
-		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Ability.Category.Action"), TEXT("ProjectD"));
-		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Ability.Category.Reaction"), TEXT("ProjectD"));
-		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Ability.Category.AdditionalAction"), TEXT("ProjectD"));
-		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Ability.Category.Combat"), TEXT("ProjectD"));
-		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Ability.Category.Spell"), TEXT("ProjectD"));
-		UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Ability.Category.Class.Mage"), TEXT("ProjectD"));
-		Move		= UGameplayTagsManager::Get().AddNativeGameplayTag(TEXT("GAS.Ability.Movement.Move"), TEXT("ProjectD"));
 	}
 
 	static bool IsDeBuff(const FGameplayTag& Tag) { return false; }
