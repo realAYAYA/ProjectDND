83a59c83d4cacc2e0baf8ff8165340fbfa845947
liudong <l1206776367@yahoo.com>
2024-07-05 01:51:05 +0800

    gameplay counter

--- Demo/Plugins/Shared/GameShared/Source/GameShared/Public/GameCounter.h
+++ Demo/Plugins/Shared/GameShared/Source/GameShared/Public/GameCounter.h
@@ -1,7 +1,33 @@
 #pragma once
+#include "CoreMinimal.h"
+#include "GameCounter.generated.h"
 
-class FGameCounter
+// 计数器层级1
+USTRUCT(BlueprintType)
+struct GAMESHARED_API FCounterLayer1
 {
-public:
-	
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY()
+	TMap<int32, int64> Map;
+};
+
+// 计数器层级2
+USTRUCT(BlueprintType)
+struct GAMESHARED_API FCounterLayer2
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY()
+	TMap<int32, FCounterLayer1> Map;
+};
+
+// 计数器层级3
+USTRUCT(BlueprintType)
+struct GAMESHARED_API FCounterLayer3
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY()
+	TMap<int32, FCounterLayer3> Map;
 };
